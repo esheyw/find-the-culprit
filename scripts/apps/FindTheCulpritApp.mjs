5b97478e4837b96e0aa8b324867529cf2645dc73
esheyw <esheyw@gmail.com>
2024-06-05 17:43:33 -0700

    functional?!

--- scripts/apps/FindTheCulpritApp.mjs
+++ scripts/apps/FindTheCulpritApp.mjs
@@ -10,11 +10,8 @@ export class FindTheCulpritApp extends FormApplication {
   constructor(object = {}, options = {}) {
     if (MODULE().app instanceof FindTheCulpritApp)
       throw new Error(game.i18n.localize("FindTheCulprit.Error.SelectModsSingleton"));
-
     super(object, options);
-
     this.#stepData = game.settings.get(MODULE_ID, "stepData");
-
     //This is to prune out modules that have a saved value in this world but are no longer installed.
     //Not sure if only my broken test world requires it, but I can't see how it'd hurt.
     const existingMods = game.modules.map((m) => m.id);
@@ -31,9 +28,8 @@ export class FindTheCulpritApp extends FormApplication {
 
   static get defaultOptions() {
     return foundry.utils.mergeObject(super.defaultOptions, {
-      title: "Find the Cuprit",
       id: "find-the-culprit-app",
-      classes: ["form", "find-the-culprit-app", "dialog"], //dialog to steal core styles
+      classes: ["form", "find-the-culprit-app"],
       width: 400,
       closeOnSubmit: false,
       submitOnChange: true,
@@ -65,20 +61,126 @@ export class FindTheCulpritApp extends FormApplication {
     return `modules/${MODULE_ID}/templates/SelectMods.hbs`;
   }
 
+  get title() {
+    return game.i18n.localize("FindTheCulprit.FindTheCulprit");
+  }
   doStep() {
     if (typeof this.#stepData.step !== "number") return;
-    if (this.#stepData === 0) return this.onlySelectedMods();
-    this.binarySearchStep();
+    if (this.#stepData.step === 0) {
+      this.onlySelectedMods();      
+    } else {
+      this.binarySearchStep();
+    }
   }
-  async onlySelectedMods() {}
 
-  async binarySearchStep() {}
+  async onlySelectedMods() {
+    this.#persists = await Dialog.confirm({
+      title: this.title,
+      content: await renderTemplate(`modules/${MODULE_ID}/templates/StartOfRun.hbs`),
+    });
+    if (this.#persists) {
+      return this.issuePeristsWithOnlySelected();
+    } else {
+      this.#stepData.step = 1;
+      await game.settings.set(MODULE_ID, "stepData", this.#stepData);
+      this.deactivationStep(this.#stepData.active);
+    }
+  }
+
+  async issuePeristsWithOnlySelected() {
+    const template = `modules/${MODULE_ID}/templates/issuePersistsWithOnlySelected.hbs`;
+    const templateData = {
+      chosen: this.#stepData.chosen.map((m) => {
+        const mod = game.modules.get(m);
+        return {
+          id: m,
+          title: mod.title,
+        };
+      }),
+    };
+    const content = await renderTemplate(template, templateData);
+    const reactivate = await Dialog.wait(
+      {
+        title: this.title,
+        content,
+        buttons: {
+          yes: {
+            icon: `<i class="fa-solid fa-arrow-rotate-left"></i>`,
+            label: game.i18n.localize("FindTheCulprit.ReactivateAllModules"),
+            callback: () => true,
+          },
+          no: {
+            icon: '<i class="fa-solid fa-xmark"></i>',
+            label: game.i18n.localize("Close"),
+            callback: () => false,
+          },
+        },
+        close: () => false,
+      },
+      { classes: ["dialog", "find-the-culprit-app"] }
+    );
+    if (reactivate) await this.reactivateModules();
+    this.resetSettings();
+  }
+
+  async binarySearchStep() {
+    const numActive = this.#stepData.active?.length || 0;
+    const numInactive = this.#stepData.inactive?.length || 0;
+    const stepsLeft = Math.ceil(Math.log2(numActive > numInactive ? numActive : numInactive)) + 1;
+    const template = `modules/${MODULE_ID}/templates/binarySearchStep.hbs`;
+    const templateData = {
+      numRemaining: numActive + numInactive,
+      stepsLeft,
+      active: this.#stepData.active.map((m) => ({
+        id: m,
+        title: game.modules.get(m).title,
+      })),
+      inactive: this.#stepData.inactive.map((m) => ({
+        id: m,
+        title: game.modules.get(m).title,
+      })),
+    };
+    const content = await renderTemplate(template, templateData);
+    Dialog.wait(
+      {
+        title: this.title,
+        content,
+        buttons: {
+          yes: {
+            icon: '<i class="fa-solid fa-check"></i>',
+            label: game.i18n.localize("Yes"),
+            callback: async function () {
+              this.deactivationStep(this.#stepData.active);
+            }.bind(this),
+          },
+          no: {
+            icon: '<i class="fa-solid fa-xmark"></i>',
+            label: game.i18n.localize("No"),
+            callback: async function () {
+              this.deactivationStep(this.#stepData.inactive);
+            }.bind(this),
+          },
+          reset: {
+            icon: '<i class="fa-solid fa-rotate-right"></i>',
+            label: game.i18n.localize("Reset"),
+            callback: async function () {
+              await this.reactivateModules();
+              this.resetSettings();
+            }.bind(this),
+          },
+        },
+        close: () => false,
+      },
+      { classes: ["dialog", "find-the-culprit-app"] }
+    );
+  }
 
   async deactivationStep(chosenModules = []) {
-    if (chosenModules.length === 1) return renderFinalDialog(chosenModules[0]);
+    if (chosenModules.length === 1) return this.renderFinalDialog(chosenModules[0]);
     const currentModList = game.settings.get("core", ModuleManagement.CONFIG_SETTING);
     //everything except the stored chosen list gets disabled
     const modsToDisable = Object.keys(currentModList).reduce((acc, mod) => {
+      if (mod === MODULE_ID) return acc;
       if (!this.#stepData.chosen.includes(mod)) acc[mod] = false;
       return acc;
     }, {});
@@ -108,22 +210,26 @@ export class FindTheCulpritApp extends FormApplication {
       culpritTitle: game.modules.get(culprit).title,
     };
     const content = await renderTemplate(template, templateData);
-    const reactivate = await Dialog.wait({
-      title: game.i18n.localize("FindTheCulprit.FindTheCulprit"),
-      content,
-      buttons: {
-        yes: {
-          icon: `<i class="fa-solid fa-list-check"></i>`,
-          label: game.i18n.localize("FindTheCulprit.ReactivateAllModules"),
-          callback: () => true,
+    const reactivate = await Dialog.wait(
+      {
+        title: game.i18n.localize("FindTheCulprit.FindTheCulprit"),
+        content,
+        buttons: {
+          yes: {
+            icon: `<i class="fa-solid fa-list-check"></i>`,
+            label: game.i18n.localize("FindTheCulprit.ReactivateAllModules"),
+            callback: () => true,
+          },
+          no: {
+            icon: '<i class="fa-solid fa-xmark"></i>',
+            label: game.i18n.localize("Close"),
+            callback: () => false,
+          },
         },
-        no: {
-          icon: '<i class="fa-solid fa-xmark"></i>',
-          label: game.i18n.localize("Close"),
-          callback: () => false,
-        },
+        close: () => false,
       },
-    });
+      { classes: ["dialog", "find-the-culprit-app"] }
+    );
     if (reactivate) await this.reactivateModules();
     this.resetSettings();
   }
@@ -134,9 +240,9 @@ export class FindTheCulpritApp extends FormApplication {
   }
 
   async startRun() {
-    console.warn("starting run", this.#selectedModules);
     this.#stepData.chosen = Object.keys(this.#selectedModules).filter((m) => this.#selectedModules[m]);
     this.#stepData.step = 0;
+    this.#stepData.active = game.modules.filter((m) => m.active && m.id !== MODULE_ID).map((m) => m.id);
     await game.settings.set(MODULE_ID, "stepData", this.#stepData);
     this.deactivationStep();
   }
@@ -163,12 +269,12 @@ export class FindTheCulpritApp extends FormApplication {
         locked: locks[m.id] || (m.library && this.#lockLibraries),
         selected: this.#selectedModules[m.id],
         library: m.library,
-      }));
+      }))
+      .sort((a, b) => a.title.localeCompare(b.title));
     return context;
   }
   async _onChangeCheckbox(event) {
     const el = event.currentTarget;
-    // console.warn(el)
     if (el.name === "lockLibraries") return;
     if (el.name.startsWith("locks")) {
       AudioHelper.play({
@@ -178,7 +284,6 @@ export class FindTheCulpritApp extends FormApplication {
     }
     const modID = el.name.split(".")[1];
     const wrongStateDeps = this.#checkDepenencies(modID) ?? [];
-    // console.warn({ modID, wrongStateDeps });
     if (wrongStateDeps.length === 0) return;
     const template = `modules/${MODULE_ID}/templates/dependenciesDialog.hbs`;
     const templateData = {
@@ -186,14 +291,12 @@ export class FindTheCulpritApp extends FormApplication {
       dependencies: wrongStateDeps,
     };
     const content = await renderTemplate(template, templateData);
-    const response = await Dialog.confirm(
-      {
-        title: game.i18n.localize("MODMANAGE.Dependencies"),
-        content,
-        yes: ([html]) => new FormDataExtended(html.querySelector("form")).object,
-        no: () => false,
-      },
-    );
+    const response = await Dialog.confirm({
+      title: game.i18n.localize("MODMANAGE.Dependencies"),
+      content,
+      yes: ([html]) => new FormDataExtended(html.querySelector("form")).object,
+      no: () => false,
+    });
     if (!response) return;
     for (const mod in response) {
       const modLockInput = this.form[`locks.${mod}`];
@@ -201,20 +304,21 @@ export class FindTheCulpritApp extends FormApplication {
       if (el.checked) {
         modInput.checked = true;
         await this.submit();
-        modInput.dispatchEvent(new Event('change'));
-      } else { //todo: improve this whole deal, dont' just quietly do nothing if locked
+        modInput.dispatchEvent(new Event("change"));
+      } else {
+        //todo: improve this whole deal, dont' just quietly do nothing if locked
         if (modLockInput.checked) continue;
         modInput.checked = false;
         await this.submit();
-        modInput.dispatchEvent(new Event('change'));
+        modInput.dispatchEvent(new Event("change"));
       }
     }
   }
+
   #checkDepenencies(modID) {
     const mod = game.modules.get(modID);
     const modCheckbox = this.form[`modules.${modID}`];
     if (!("requires" in mod.relationships) || !mod.relationships.requires.size) return;
-    console.warn(`${modID} has dependencies`);
     return mod.relationships.requires.reduce((acc, rel) => {
       const depInstalled = game.modules.get(rel.id);
       if (!depInstalled) {
@@ -239,6 +343,7 @@ export class FindTheCulpritApp extends FormApplication {
       return acc;
     }, []);
   }
+
   activateListeners(jq) {
     super.activateListeners(jq);
     const html = jq[0];
@@ -263,7 +368,6 @@ export class FindTheCulpritApp extends FormApplication {
   }
 
   async _updateObject(event, formData) {
-    console.warn("updateObject");
     formData = fu.expandObject(new FormDataExtended(this.form, { disabled: true }).object);
     this.#lockLibraries = formData.lockLibraries;
     await game.settings.set(MODULE_ID, "lockLibraries", this.#lockLibraries);
